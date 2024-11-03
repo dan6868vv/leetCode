878e121a6e2517cb79d99254a48ace5f52deadfd
Danila Zubar <dan6868vv@gmail.com>
2024-11-04 00:36:55 +0300

    Reverce-linked-list-EASY

--- main.cpp
+++ main.cpp
@@ -18,12 +18,28 @@ struct ListNode {
 class Solution {
 public:
     ListNode *reverseList(ListNode *head) {
-        if (head->next->next == nullptr) {
-            head->next->next = head;
-            head->next = nullptr;
-        } else if (head->next == nullptr) {
-            
-        } else reverseList(head->next);
+        if (head == nullptr) return nullptr;
+        if(head->next==nullptr) return head;
+        if(head->next->next==nullptr) {
+            ListNode *node = head->next;
+            head->next->next=head;
+            head->next=nullptr;
+            return node;
+        }
+        ListNode *node1 = head;
+        ListNode *node2 = head->next;
+        ListNode *node3 = node2->next;
+        while (node3->next != nullptr) {
+            node2->next = node1;
+            node1 = node2;
+            node2 = node3;
+            node3 = node3->next;
+        }
+        node2->next = node1;
+        node3->next = node2;
+        head->next = nullptr;
+        return node3;
+
     }
 
     void printList(ListNode *head) {
@@ -38,8 +54,8 @@ public:
 int main() {
     ListNode node5(5);
     ListNode node4(4, &node5);
-    ListNode node3(3, &node4);
-    ListNode node2(2, &node3);
+    ListNode node3(3,&node4);
+    ListNode node2(2,&node3);
     ListNode head(1, &node2);
     Solution sol;
     sol.printList(&head);
