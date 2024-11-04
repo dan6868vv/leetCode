7b1a36627b37e6fc6dd8771710472a8e60d7d82f
Danila Zubar <dan6868vv@gmail.com>
2024-11-04 11:16:05 +0300

    Single-number-EASY-Solved

--- main.cpp
+++ main.cpp
@@ -1,65 +1,33 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
-
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    ListNode() : val(0), next(nullptr) {
-    }
-
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
-};
 
 class Solution {
 public:
-    ListNode *reverseList(ListNode *head) {
-        if (head == nullptr) return nullptr;
-        if(head->next==nullptr) return head;
-        if(head->next->next==nullptr) {
-            ListNode *node = head->next;
-            head->next->next=head;
-            head->next=nullptr;
-            return node;
-        }
-        ListNode *node1 = head;
-        ListNode *node2 = head->next;
-        ListNode *node3 = node2->next;
-        while (node3->next != nullptr) {
-            node2->next = node1;
-            node1 = node2;
-            node2 = node3;
-            node3 = node3->next;
-        }
-        node2->next = node1;
-        node3->next = node2;
-        head->next = nullptr;
-        return node3;
+    int singleNumber(std::vector<int>& nums) {
 
+        if(nums.size()==1) return nums[0];
+
+        if(nums[1]-nums[0]!=0) return nums[0];
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size()-1;i++) {
+            if(nums[i-1] - nums[i] != 0 && nums[i+1] - nums[i] != 0) return nums[i];
+
+        }
+        return nums[nums.size()-1];
     }
 
-    void printList(ListNode *head) {
-        std::cout << head->val << " ";
-        while (head->next != nullptr) {
-            head = head->next;
-            std::cout << head->val << " ";
-        }
-    }
 };
 
 int main() {
-    ListNode node5(5);
-    ListNode node4(4, &node5);
-    ListNode node3(3,&node4);
-    ListNode node2(2,&node3);
-    ListNode head(1, &node2);
     Solution sol;
-    sol.printList(&head);
-    std::cout << std::endl;
-    sol.printList(sol.reverseList(&head));
+    std::vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(2);
+    nums.push_back(1);
+    // nums.push_back(2);
+    // nums.push_back(2);
+    std::cout << sol.singleNumber(nums);
     return 1;
 }
