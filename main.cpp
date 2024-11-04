1e39514cd100d7f8ece54fc9f4f2e1835dc03135
Danila Zubar <dan6868vv@gmail.com>
2024-11-04 12:53:33 +0300

    Pascals-triangle-EASY-Solved

--- main.cpp
+++ main.cpp
@@ -5,18 +5,19 @@
 
 class Solution {
 public:
-    std::vector<std::vector<int>> generate(int numRows) {
-        std::vector<std::vector<int>> returnVectors;
-        if(numRows==0) return returnVectors;
+    std::vector<std::vector<int> > generate(int numRows) {
+        std::vector<std::vector<int> > returnVectors;
+        if (numRows == 0) return returnVectors;
         std::vector<int> firstVector;
         firstVector.push_back(1);
         returnVectors.push_back(firstVector);
-        if(numRows==1) return returnVectors;
+        if (numRows == 1) return returnVectors;
         std::vector<int> secondVecor;
         secondVecor.push_back(1);
         secondVecor.push_back(1);
-
-        for(int i=1;i<numRows;i++) {
+        returnVectors.push_back(secondVecor);
+        if (numRows == 2) return returnVectors;
+        for (int i = 1; i < numRows - 1; i++) {
             returnVectors.push_back(getNextVector(returnVectors[i]));
         }
         return returnVectors;
@@ -25,8 +26,8 @@ public:
     std::vector<int> getNextVector(std::vector<int> nums1) {
         std::vector<int> nums2;
         nums2.push_back(1);
-        for(int i=0;i<nums1.size()-1;i++) {
-            nums2.push_back(nums1[i]+nums2[i+1]);
+        for (int i = 0; i < nums1.size() - 1; i++) {
+            nums2.push_back(nums1[i] + nums1[i + 1]);
         }
         nums2.push_back(1);
         return nums2;
@@ -35,11 +36,11 @@ public:
 
 int main() {
     Solution sol;
-    std::vector<std::vector<int>> nums;
-    nums = sol.generate(5);
-    for(int i=0;i<nums.size();i++) {
-        for(int j=0;j < nums[i].size();j++) {
-            std::cout << nums[i][j] << " ";
+    std::vector<std::vector<int> > nums;
+    nums = sol.generate(10);
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = 0; j < nums[i].size(); j++) {
+            std::cout << " " << nums[i][j] << " ";
         }
         std::cout << "|";
     }
