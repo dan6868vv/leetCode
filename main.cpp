4529968921b5abe0786a085442c68e26ec10d9a9
Danila Zubar <dan6868vv@gmail.com>
2024-11-04 13:36:27 +0300

    Merge-two-sorted-lists-EASY-NOSOLVED

--- main.cpp
+++ main.cpp
@@ -1,48 +1,61 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {
+    }
+
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {
+    }
+};
 
 class Solution {
 public:
-    std::vector<std::vector<int> > generate(int numRows) {
-        std::vector<std::vector<int> > returnVectors;
-        if (numRows == 0) return returnVectors;
-        std::vector<int> firstVector;
-        firstVector.push_back(1);
-        returnVectors.push_back(firstVector);
-        if (numRows == 1) return returnVectors;
-        std::vector<int> secondVecor;
-        secondVecor.push_back(1);
-        secondVecor.push_back(1);
-        returnVectors.push_back(secondVecor);
-        if (numRows == 2) return returnVectors;
-        for (int i = 1; i < numRows - 1; i++) {
-            returnVectors.push_back(getNextVector(returnVectors[i]));
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        if (list1 == nullptr && list2 == nullptr) return nullptr;
+        if (list1 == nullptr || list2 == nullptr) return list1 == nullptr ? list2 : list1;
+        ListNode *node = list1->val < list2->val ? list1 : list2;
+        while (list2->next != nullptr || list1->next != nullptr) {
+            if (list2->val < list1->val) {
+                node->next = list1;
+                list1 = list1->next;
+            } else {
+                node->next = list2;
+                list2 = list2->next;
+            }
         }
-        return returnVectors;
+        if ((list2->next != nullptr || list1->next != nullptr) && !(list2->next != nullptr && list1->next != nullptr)) {
+            return node->next = list1->next == nullptr ? list2 : list1;
+        }
+        return node;
     }
 
-    std::vector<int> getNextVector(std::vector<int> nums1) {
-        std::vector<int> nums2;
-        nums2.push_back(1);
-        for (int i = 0; i < nums1.size() - 1; i++) {
-            nums2.push_back(nums1[i] + nums1[i + 1]);
+    void coutListNode(ListNode *list) {
+        while (list != nullptr) {
+            std::cout << list->val << " ";
+            list = list->next;
         }
-        nums2.push_back(1);
-        return nums2;
     }
 };
 
 int main() {
     Solution sol;
-    std::vector<std::vector<int> > nums;
-    nums = sol.generate(10);
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = 0; j < nums[i].size(); j++) {
-            std::cout << " " << nums[i][j] << " ";
-        }
-        std::cout << "|";
-    }
+    ListNode node3(4);
+    ListNode node2(2, &node3);
+    ListNode node1(1, &node2);
+    ListNode node6(4);
+    ListNode node5(3, &node6);
+    ListNode node4(1, &node5);
+    sol.coutListNode(&node1);
+    std::cout << '\n';
+    sol.coutListNode(&node4);
+    std::cout << '\n';
+    sol.coutListNode(sol.mergeTwoLists(&node1, &node4));
     return 1;
 }
