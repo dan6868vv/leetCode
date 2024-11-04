33fb7c86fe6330574fc666ecdafa80d24888438b
Danila Zubar <dan6868vv@gmail.com>
2024-11-04 18:38:17 +0300

    Linked-list-cycle

--- main.cpp
+++ main.cpp
@@ -2,31 +2,45 @@
 #include <iostream>
 #include <vector>
 
+
+struct ListNode {
+public:
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {
+    }
+};
+
 class Solution {
 public:
-    void moveZeroes(std::vector<int> &nums) {
-        int i = 0,j=0;
-        while (j<nums.size()) {
-            j++;
-            if(nums[i]==0) {
-                nums.push_back(0);
-                nums.erase(nums.begin()+i);
-            }
-            else i++;
+    bool hasCycle(ListNode *head) {
+        if(head->next==nullptr) return false;
+        std::vector<std::reference_wrapper<ListNode>> refList;
+        int i=0;
+        while(head->next!=nullptr) {
+            if(i>10000) return true;
+            i++;
+            head=head->next;
+
         }
+        return false;
     }
 };
 
 int main() {
     Solution sol;
-    std::vector<int> nums = {0, 0, 0, 0, 0, 3};
-    for (int n: nums) {
-        std::cout << n << " ";
-    }
-    std::cout << '\n';
-    sol.moveZeroes(nums);
-    for (int n: nums) {
-        std::cout << n << " ";
-    }
+
+    ListNode node2(2);
+    ListNode node0(0);
+    ListNode node4(4);
+    ListNode node3(3);
+    node2.next=&node0;
+    node0.next=&node4;
+    node4.next=&node3;
+    node3.next=&node2;
+    // std::cout << sol.hasCycle(&node3) ? "True" : "Flase";
+    if(sol.hasCycle(&node3)) std::cout << "TRUE" << std::endl;
+    else std::cout << "FALSE" << std::endl;
     return 1;
 }
